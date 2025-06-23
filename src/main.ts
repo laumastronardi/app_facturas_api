1971138cf4c2965519a3865010ee778c64c42567
Lautaro Mastronardi <lautaromastronardi@gmail.com>
2025-06-23 20:01:09 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
   
-
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port ?? 3000, '0.0.0.0');
 
   console.log(`🚀 Application listening on http://0.0.0.0:${port}`);
 }
