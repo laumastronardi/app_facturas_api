fa61a23113722af8ba84f30500187268bca3d58c
Lautaro Mastronardi <lautaromastronardi@gmail.com>
2025-06-25 20:16:39 -0300

    changes in index.ts

--- api/index.ts
+++ api/index.ts
@@ -4,21 +4,23 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 import serverless from 'serverless-http';
-import { AppModule } from '../src/app.module'; // <-- Import relativo
+import { AppModule } from '../src/app.module'; // Import relativo
 
 const server = express();
-let app: any;
+let isBootstrapped = false;
 
 async function bootstrap() {
-  if (!app) {
-    app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  if (!isBootstrapped) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
     app.enableCors();
     await app.init();
+    isBootstrapped = true;
   }
-  return app;
 }
 
-export default serverless(async (req, res) => {
+const handler = serverless(async (req, res) => {
   await bootstrap();
   return server(req, res);
 });
+
+export default handler;
