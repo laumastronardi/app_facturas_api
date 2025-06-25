02c1d7a0a1b23173d1fc97a214041f6d473040f8
Lautaro Mastronardi <lautaromastronardi@gmail.com>
2025-06-25 20:38:26 -0300

    remove TypeORM import

--- src/modules/invoice/invoice.module.ts
+++ src/modules/invoice/invoice.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Invoice } from './entities/invoice.entity';
 import { InvoiceService } from './services/invoice.service';
 import { InvoiceController } from './controllers/invoice.controller';
-import { Supplier } from '../supplier/entities/supplier.entity';
 import { SupabaseModule } from 'src/supabase/supabase.module';
 
 @Module({
