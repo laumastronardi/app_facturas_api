f485121e10f392e16f08dec0830de242cba6fdfd
Lautaro Mastronardi <lautaromastronardi@gmail.com>
2025-06-25 21:41:59 -0300

    fixes

--- api/index.ts
+++ api/index.ts
@@ -122,8 +122,8 @@ app.get('/invoices/:id', async (req: Request, res: Response) => {
   }
 });
 
-// 404 handler
-app.use('*', (req: Request, res: Response) => {
+// 404 handler (no wildcard)
+app.use((req: Request, res: Response) => {
   res.status(404).json({
     error: 'Route not found',
     availableRoutes: [
