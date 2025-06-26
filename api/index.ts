e61d2092023fa83c77d514d10c608313b369959d
Lautaro Mastronardi <lautaromastronardi@gmail.com>
2025-06-25 21:03:56 -0300

    fix

--- api/index.ts
+++ api/index.ts
@@ -7,22 +7,22 @@ import express from 'express';
 import serverless from 'serverless-http';
 import { AppModule } from '../src/app.module';
 
-const server = express();
-let isBootstrapped = false;
+let nestApp: any = null;
 
 async function bootstrap() {
-  if (!isBootstrapped) {
-    console.log('Bootstrapping Nest app...');
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-    app.enableCors();
-    await app.init();
-    isBootstrapped = true;
+  if (!nestApp) {
+    const expressInstance = express();
+    nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+    nestApp.enableCors();
+    await nestApp.init();
+    (nestApp as any).expressInstance = expressInstance;
     console.log('Nest app bootstrapped');
   }
+  return (nestApp as any).expressInstance;
 }
 
 export default serverless(async (req, res) => {
   console.log('Request received');
-  await bootstrap();
-  return server(req, res);
+  const expressInstance = await bootstrap();
+  return expressInstance(req, res);
 });
